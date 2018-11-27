dea3da123fbd57269437784635836f57c68a0d8e
K Hess <khess013@gmail.com>
2018-11-27 09:06:24 -0500

    update to file path for csv

--- code/NewForm_3.py
+++ code/NewForm_3.py
@@ -18,7 +18,7 @@ warnings.simplefilter('ignore')
 fiscalYear = input('What year?')
 if len(fiscalYear) < 1: input('Enter a year, dummy.')
 
-inputpath = '/Users/kellyhess/LGDR/'
+inputpath = '/Users/kellyhess/LGDR/code/'
 
 if fiscalYear != 'test':
 	inputpath = inputpath + fiscalYear +'/'
